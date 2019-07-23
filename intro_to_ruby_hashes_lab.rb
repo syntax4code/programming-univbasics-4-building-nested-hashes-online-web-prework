55ee75a0dc6f9cdb205189b27241dfc1d20aba0f
Warren Loy Harper II <warrenharper2@att.net>
2019-07-23 22:32:29 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -42,7 +42,26 @@ monopoly = {:railroads=>
       :three_pieces_owned=>100,
       :four_pieces_owned=>200
     },
-    :names=> 
+    :names=>
+    {
+      :reading_railroad=>
+      {
+        "mortgage_value"=>"$100"
+      },
+      :pennsylvania_railroad=>
+      {
+        "mortgage_value"=>"$200"
+      },
+      :b_and_o_railroad=>
+      {
+        "mortgage_value"=>"$400"
+      },
+      :shortline=>
+      {
+        "mortgage_value"=>"$800"
+      }
+    }
+  }
+}
     
 
-end
